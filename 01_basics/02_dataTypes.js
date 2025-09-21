24fef054bee6269f6600d2bd3aafcc11a95d5ca8
RohanLodhi01 <rohanlodhi51@gmail.com>
2025-09-21 08:02:34 +0000

    datatypes done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version of js
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +3)  // code readability high
+
+console.log("rohan")
+
+
+let name = "rohan"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+// number => range 2 to pwer 53
+// bigint
+// string = ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined);
+console.log(typeof null);
+
