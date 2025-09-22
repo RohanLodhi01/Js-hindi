83dcef7edf6c282a4a0e52958a49127b9e100a29
RohanLodhi01 <rohanlodhi51@gmail.com>
2025-09-22 13:05:50 +0000

    comparisons updated

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -7,13 +7,17 @@
 // console.log("2" > 1);
 // console.log("02" > 1)
 
-console.log(null > 0);
+console.log(null > 0);       /* avoid these type of conditions*/
 console.log(null == 0);
 console.log(null >= 0);
-
+                                        
 console.log(undefined == 0);
 console.log(undefined > 0);
 console.log(undefined < 0);
 
+
+// ===
+console.log("2" === 2); // it checks without conversion of datatypes
+// checks string to number above
 
 
