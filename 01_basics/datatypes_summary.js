c0df1406bfe23909ed5dfc61b7c765aaf5034127
RohanLodhi01 <rohanlodhi51@gmail.com>
2025-09-22 14:26:34 +0000

    datatypes summary added

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,57 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 23245585837384748n
+
+// Refrence (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktimaan","naagraj","doga"];
+
+let myObj = {
+    name: "rohan",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World!");
+}
+
+// console.log(typeof score);
+// console.log(typeof scoreValue);
+// console.log(typeof isLoggedIn);
+// console.log(typeof outsideTemp);
+// console.log(typeof userEmail);
+// console.log(typeof id);
+// console.log(typeof anotherId);
+// console.log(typeof bigNumber);
+
+//  https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubename = "rohanlodhidotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
