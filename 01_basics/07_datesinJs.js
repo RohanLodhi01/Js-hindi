0bf0a28e283a4361dcd39f33f65648dd70419f72
RohanLodhi01 <rohanlodhi51@gmail.com>
2025-09-23 15:30:12 +0000

    .

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,24 @@
+// Dates
+
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2025, 0, 23)
+// let myCreatedDate = new Date(2025, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2025-01-14")
+let myCreatedDate = new Date("01-14-2025")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStanp = Date.now()
+
+console.log(myTimeStanp);
+
+
+
